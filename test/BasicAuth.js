90757b86cb842a3735b770d3960000f97f657e1a
Eddie Saunders <saunders.eddie@outlook.com>
2021-02-03 18:25:01 -0500

    Added in some alert testing

--- test/BasicAuth.js
+++ test/BasicAuth.js
@@ -1,25 +1,27 @@
 import { Key, until } from 'selenium-webdriver';
 import {openDriver, openPage, closeBrowser, screenShot, clickByXPath, clickByCSS} from '../libs/basePage.js';
 
-const ourDriver = await openDriver("firefox")
+const ourDriver = await openDriver("chrome")
 const ourTestPage = "http://the-internet.herokuapp.com/"
 const ourUserName = "admin";
 const ourPassword = "admin";
 const ourBadUserName = "Bad User";
 const ourBadPassword = "Bad Password";
 
-// await ourDriver.manage().window().maximize();
+const myAlert = async (myDriver, testTect) => {
+    try {
+        await ourDriver.wait(until.alertIsPresent());
+        console.log ("Alert is present");
+        let ourAlert = await ourDriver.switchTo().alert();
+        await ourAlert.sendKeys("admin");
+    } catch (error) {
+        console.log ("Error with prompt: ", error)
+    }
+
+}
+
 
 await openPage (ourDriver,ourTestPage);
 await clickByXPath(ourDriver, "/html/body/div[2]/div/ul/li[3]/a");
-await ourDriver.sleep(10000);
-// await ourDriver.wait(until.alertIsPresent());
-let ourAlert = await ourDriver.switchTo().alert();
-console.log (ourAlert.getText());
 
-await ourDriver.sendKeys("admin");
-
-// Now we have a pop up
-// await ourDriver.wait(until.alertIsPresent().sendKeys("admin", Key.TAB, "admin"));
-// await ourDriver.switchTo().alert().sendKeys(ourUserName + Key.TAB + ourPassword );
-// await closeBrowser(ourDriver);
+let myAlertTest = await myAlert();
