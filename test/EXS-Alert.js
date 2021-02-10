7080a337624adc0f88ba60792f069ae57e2e64e3
Eddie Saunders <saunders.eddie@outlook.com>
2021-02-10 06:50:26 -0500

    Added in simple alert test

---
+++ test/EXS-Alert.js
@@ -0,0 +1,28 @@
+//  https://saunderseddie.github.io/testing-site/
+// EXS 9th February 2021 
+// Simple demo of going to a page click a button and then accept the alert prompt.
+
+// User Story:
+// As a user I wish to open a website
+// Then click the Alert link
+// I will then accept the alert regardless of any text displayed
+
+
+import {openDriver, 
+        openPage, 
+        closeBrowser,
+        clickByXPath,
+        acceptAlert,
+        clickByName } from '../libs/basePage.js';
+
+const ourDriver = await openDriver("firefox")
+const ourTestPage = "https://saunderseddie.github.io/testing-site/"
+
+
+await ourDriver.manage().setTimeouts({implicit: 10000});
+
+await openPage (ourDriver,ourTestPage);
+await clickByXPath(ourDriver,"/html/body/ul/li[3]/a");
+await acceptAlert(ourDriver);
+await clickByName(ourDriver,"goHome");
+await closeBrowser(ourDriver);
