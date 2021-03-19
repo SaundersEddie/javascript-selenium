ea792bcb7f28c705834d9d002df0f28d30dc69a5
Eddie Saunders <saunders.eddie@outlook.com>
2021-03-19 08:49:59 -0400

    upgraded selenium to v4.0.0-beta.2

--- pageObjects/homePage.js
+++ pageObjects/homePage.js
@@ -6,7 +6,6 @@ class HomePage extends BasePage{
     enter_search(searchText){
         driver.findElement(By.css('input[name=q]')).sendKeys(searchText);
         driver.findElement(By.css('input[name=q]')).sendKeys(Key.ENTER);
-        // driver.quit();
      }
 }
 
